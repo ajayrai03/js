71c0ffbc6eb4a6a65beef8532c09b3855eafcf75
ajayrai03 <ajayrai1012@gmail.com>
2022-10-08 22:02:46 +0530

    js fundamental

--- js_info.js
+++ js_info.js
@@ -1,1 +1,45 @@
-//hello
+// data type
+let str="ajay";
+// console.log(str);
+// alert(`learning js ${str}`);
+// the undefined
+// let age;
+// console.log(typeof(age));
+// let a=10;
+// console.log(typeof(a));
+// let hour=12;
+// let minute=30;
+// if(hour==12&&minute==30){
+//     console.log("The time is 12:30");
+// }
+// let year=prompt("in which year ecmascript published");
+// if(year==2015){
+//     alert("you guessed it right");
+// }
+// else{
+//     alert("oops, next time");
+// }
+// let age=16;
+// if(age>=18?true:false){
+//     console.log(12);
+// }
+// else{
+//     console.log("fg");
+// }
+let a=2+2;
+switch(a){
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    case 4:
+        console.log(4);
+        break;
+    default:
+        log("nice attempt");
+
+}
+console.log("hurray!");
+// next function 
