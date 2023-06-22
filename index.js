11084495283131a0854e70cede70122c4e3c92d3
ajayrai03 <ajayrai1012@gmail.com>
2023-06-22 20:39:33 +0530

    function

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // console.log(2+2);
-function msg(){
-    console.log("hello js!");
+// function msg(){
+//     console.log("hello js!");
+// }
+// console.log(msg());
+
+let a=10;
+let b=25;
+if(a<b){
+    console.log("smaller");
 }
-console.log(msg());
+else{
+    console.log("not smaller");
+}
