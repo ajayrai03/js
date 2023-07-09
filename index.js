a3661231e22413eaaaec7b5cecef4993a13cde7c
ajayrai03 <ajayrai1012@gmail.com>
2023-07-09 20:16:10 +0530

    javascript interview question revision

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// console.log(2+2);
-// function msg(){
-//     console.log("hello js!");
-// }
-// console.log(msg());
-
-let a=10;
-let b=25;
-if(a<b){
-    console.log("smaller");
-}
-else{
-    console.log("not smaller");
-}
+// String
+var str="Vivek Singh Bisht";
+var str2='John Doe';
